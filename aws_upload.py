292864b51df21220ac2708ba0a593946343489a6
aosborne17 <AOsborne@spartaglobal.com>
2020-09-02 18:59:51 +0100

    S3 bucket connection

--- aws_upload.py
+++ aws_upload.py
@@ -1,21 +1,25 @@
+
 import boto3
 from botocore.exceptions import NoCredentialsError
 import os
 
 
-def upload_to_aws(local_file, bucket, s3_file):
-    aws_access_key = os.environ["AWS_ACCESS_KEY"]
-    aws_secret_key = os.environ["AWS_SECRET_KEY"]
-    s3 = boto3.client('s3',aws_access_key,
-                      aws_secret_key)
+# def upload_to_aws(local_file, bucket, s3_file):
+    
+    # s3 = boto3.client('s3',aws_access_key,
+    #                   aws_secret_key)
 
-    try:
-        s3.upload_file(local_file, bucket, s3_file)
-        print("Upload Successful")
-        return True
-    except FileNotFoundError:
-        print("The file was not found")
-        return False
-    except NoCredentialsError:
-        print("Credentials not available")
-        return False
+    # try:
+    #     s3.upload_file(local_file, bucket, s3_file)
+    #     print("Upload Successful")
+    #     return True
+    # except FileNotFoundError:
+    #     print("The file was not found")
+    #     return False
+    # except NoCredentialsError:
+    #     print("Credentials not available")
+    #     return False
+
+
+    #have all the variables populated which are required below
+  
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import boto3
 from botocore.exceptions import NoCredentialsError
 import os
 from aws_upload import upload_to_aws # importing the function that will upload file to aws
+from boto3.s3.transfer import S3Transfer
 
 # the below functions will be run when we run our python main.py in the shell
 if __name__ == '__main__':
@@ -14,8 +15,14 @@ if __name__ == '__main__':
     Top30CSVGenerator().generate_top_30_csv(top_30.get_top_30_table_elements_into_array(), os.path.expanduser('Downloads/'), 'ItJobsWatchTop30.csv', top_30.get_table_headers_array())
 
     # this function will take our csv file that will be stored in our downloads folder and push it to our s3 bucket, the details are specified below
-    upload_to_aws('Downloads/ItJobsWatchTop30.csv', 'eng-67-shahrukh-devops', 'ItJobsWatchTop30.csv')
+    # upload_to_aws('Downloads/ItJobsWatchTop30.csv', 'eng-67-shahrukh-devops', 'ItJobsWatchTop30.csv')
 
 
 
 ## making changes to merge
+    aws_access_key = os.environ["AWS_ACCESS_KEY"]
+    aws_secret_key = os.environ["AWS_SECRET_KEY"]
+
+    client = boto3.client('s3', aws_access_key, aws_secret_key)
+    transfer = S3Transfer(client)
+    transfer.upload_file('Downloads/ItJobsWatchTop30.csv', 'eng-67-shahrukh-devops', ItJobsWatchTop30.csv)
